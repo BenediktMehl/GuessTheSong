f87308da76067a64568c9af61058311f92fb83a8
Benedikt Mehl <b.mehl14@outlook.de>
2025-11-04 18:47:04 +0100

    fix dark theme readability

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: ["./index.html", "./src/**/*.{ts,tsx}"];
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["light"],
+        darkTheme: "light",
+        base: true,
+        styled: true,
+        utils: true,
+        logs: false,
+    },
+};
+
+export default config;
